9359ef4b4d317ec2c471d8db1545f9d643f9efc2
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-04-06 16:37:49 +0100

    final product

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# bootcamp-module5-challenge
+# Bootcamp Module5 Challenge - Password generator
+
+## Description
+This small web application generates a random password based on criteria the user selected.
+
+## Criteria
+* Generate a password when the button is clicked
+  * Present a series of prompts for password criteria
+    * Length of password
+      * At least 8 characters but no more than 128.
+    * Character types
+      * Lowercase
+      * Uppercase
+      * Numeric
+      * Special characters ($@%&*, etc)
+* Code validates for each input and at least one character type should be selected
+* Once prompts are answered then the password is generated and written to the page
+
+## Live URL
+TODO
+
+## Usage
+1. On the welcome page click on Generate Password button
+![password generator demo](./images/05-javascript-challenge-demo.png)
+
+2. First asks for the password length. Please enter a number between 8 and 128. If you entered a wrong number you can't carry on
+
+![password generator demo](./images/step-1.png)
+
+3. Then ask whether the password should contain lowercase characters
+
+![password generator demo](./images/step-2.png)
+
+4. Then ask whether the password should contain uppercase characters
+
+![password generator demo](./images/step-3.png)
+
+5. Then ask whether the password should contain numeric characters
+
+![password generator demo](./images/step-4.png)
+
+6. Then ask whether the password should contain special characters
+
+![password generator demo](./images/step-5.png)
+
+7. The generated password is ready
+
+![password generator demo](./images/step-6.png)
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -88,19 +88,132 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var passwordCriteria = {
+  length: 0,
+  isLowercase: false,
+  isUpperCase: false,
+  isNumeric: false,
+  isSpecialChars: false,
+  minLength: 8,
+  maxLength: 128
+}
+
+//this is a 2D array which could store the lowercase, uppercase, numeric or special character arrays
+var bigArray = [];
+
+function initVars() {
+  bigArray = [];
+  passwordCriteria.isLowercase = false;
+  passwordCriteria.isUpperCase = false;
+  passwordCriteria.isNumeric = false;
+  passwordCriteria.isSpecialChars = false;
+}
+
+//check if the input is a number
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+
+//display and validate the password length prompt.
+function validatePasswordLength() {
+  var retVal = true;
+  passwordCriteria.length = prompt("Please enter the length of the generated password. The number should be between 8 and 128");
+
+  if(isNumber(passwordCriteria.length)) {
+    if(passwordCriteria.length >= passwordCriteria.minLength && passwordCriteria.length <= passwordCriteria.maxLength) {
+      retVal = true;
+    } else {
+      retVal = false;
+      alert("The number should be between 8 and 128");
+    }
+  } else {
+    retVal = false;
+    alert("Please enter a valid number");
+  }
+
+  return retVal;
+}
+
+//display a series of confirm popups for the characters to be included to the password
+function validateUserInputs() {
+  var retVal = true;
+
+  passwordCriteria.isLowercase = confirm("Do you want to include lowercase characters?");
+  passwordCriteria.isUpperCase = confirm("Do you want to include uppercase characters?");
+  passwordCriteria.isNumeric = confirm("Do you want to include numeric characters?");
+  passwordCriteria.isSpecialChars = confirm("Do you want to include special characters?");
+
+
+  if(!passwordCriteria.isLowercase && !passwordCriteria.isUpperCase && !passwordCriteria.isNumeric && !passwordCriteria.isSpecialChars) {
+    alert("Please select yes to at least one option");
+    retVal = false;
+  }
+
+  return retVal;
+}
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var retVal = false;
+  retVal = validatePasswordLength();
 
+  if(retVal) {
+    retVal = validateUserInputs();
+  }
+  return retVal;
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  return arr[Math.floor((Math.random()*arr.length))];
+}
 
+
+//this will give an all-in one 2D array so we can randomly select the types and the characters
+function createBigArray() {
+  if (passwordCriteria.isLowercase) {
+    bigArray.push(lowerCasedCharacters);
+  }
+
+  if (passwordCriteria.isUpperCase) {
+    bigArray.push(upperCasedCharacters);
+  }
+
+  if (passwordCriteria.isNumeric) {
+    bigArray.push(numericCharacters);
+  }
+
+  if (passwordCriteria.isSpecialChars) {
+    bigArray.push(specialCharacters);
+  }
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  //we have to initialise the variables each time we click on the button
+  //otherwise unexpected behaviours will happen
+  initVars();
 
+  var finalPassword = "";
+  if(getPasswordOptions()) {
+
+    createBigArray();
+
+    //iterate through the chosen password length
+    for(var i=0; i<passwordCriteria.length; i++) {
+      //randomly select the array we work from (this could be lowecase, uppercase, number and special)
+      selectedArray = getRandom(bigArray);
+
+      //randomly select a character from the array
+      selectedChar = getRandom(selectedArray);
+
+      //concatenating the password string
+      finalPassword = finalPassword.concat(selectedChar);
+    }
+  }
+
+  return finalPassword;
 }
 
 // Get references to the #generate element
